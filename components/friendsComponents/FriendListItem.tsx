d8d2dbd18f9e08a5578aafaed1b790cf1c800149
KesavCodes <kesavakumarcode@outlook.com>
2025-06-12 21:23:45 +0530

    [Extracting the List item logic for reusability]

--- components/friendsComponents/FriendListItem.tsx
+++ components/friendsComponents/FriendListItem.tsx
@@ -1,16 +1,44 @@
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import { FriendsResponse } from "../QuestionComponents/PreviewQuestion/FriendDrawer";
 import { colors } from "../../constants/colors";
+import LoadingState from "../../assets/images/ui/Loading";
+import EmptyState from "../../assets/images/ui/Empty";
+import RemoveFriendOption from "./RemoveFriendOption";
 
-interface FriendListItemProps {
+interface FriendsListProps {
+  friendsData: FriendsResponse[];
+  isLoading: boolean;
+  showEmptyState?: boolean;
+}
+
+interface FriendItemProps {
   friendData: FriendsResponse;
   ctaComponent?: React.ReactNode;
 }
 
-const FriendListItem = ({ friendData, ctaComponent }: FriendListItemProps) => {
-  const displayName = friendData.name || friendData.username;
+const LoadingFriends = () => (
+  <View>
+    <View style={styles.imgHolder}>
+      <LoadingState />
+    </View>
+    <Text style={styles.feedbackText}>
+      Sit tight, We are loading your Friends
+    </Text>
+  </View>
+);
 
+const EmptyFriends = () => (
+  <View>
+    <View style={styles.imgHolder}>
+      <EmptyState />
+    </View>
+    <Text style={styles.feedbackText}>No friends found</Text>
+  </View>
+);
+
+const FriendItem = ({ friendData, ctaComponent }: FriendItemProps) => {
+  const displayName = friendData.name || friendData.username;
   return (
     <View style={styles.container}>
       <View style={styles.card}>
@@ -26,6 +54,65 @@ const FriendListItem = ({ friendData, ctaComponent }: FriendListItemProps) => {
       </View>
       {ctaComponent}
     </View>
+  );
+};
+
+const FriendListItem = ({
+  friendsData,
+  isLoading,
+  showEmptyState = true,
+}: FriendsListProps) => {
+  const [showOptions, setShowOptions] = useState<Record<string, boolean>>({});
+  const [prevSelectedId, setPrevSelectedId] = useState<string | null>(null);
+
+  const optionClickHandler = (id: string) => {
+    setShowOptions((prevState) => {
+      if (prevSelectedId && prevSelectedId !== id) {
+        prevState[prevSelectedId] = false;
+      }
+      prevState[id] = !prevState[id];
+      return { ...prevState };
+    });
+    setPrevSelectedId((prevId) => (prevId === id ? null : id));
+  };
+
+  useEffect(() => {
+    setShowOptions(
+      friendsData.reduce(
+        (acc: Record<string, boolean>, friend: FriendsResponse) => {
+          acc[friend.id] = false;
+          return acc;
+        },
+        {}
+      )
+    );
+  }, []);
+
+  return (
+    <FlatList
+      data={friendsData}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <FriendItem
+          friendData={item}
+          ctaComponent={
+            <RemoveFriendOption
+              id={item.id}
+              showOptions={showOptions[item.id]}
+              optionClickHandler={optionClickHandler}
+            />
+          }
+        />
+      )}
+      contentContainerStyle={styles.contentContainer}
+      ListEmptyComponent={
+        isLoading ? (
+          <LoadingFriends />
+        ) : showEmptyState ? (
+          <EmptyFriends />
+        ) : undefined
+      }
+    />
   );
 };
 
@@ -58,5 +145,17 @@ const styles = StyleSheet.create({
     width: 50,
     borderRadius: 25,
     borderWidth: 1.5,
+  },
+  imgHolder: {
+    maxHeight: 450,
+  },
+  feedbackText: {
+    textAlign: "center",
+    color: colors.textPrimary,
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  contentContainer: {
+    paddingVertical: 8,
   },
 });
--- screens/Friends/AllFriendsScreen.tsx
+++ screens/Friends/AllFriendsScreen.tsx
@@ -1,51 +1,14 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import { getFriends } from "../../api/friends";
 import { FriendsResponse } from "../../components/QuestionComponents/PreviewQuestion/FriendDrawer";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import FriendListItem from "../../components/friendsComponents/FriendListItem";
-import RemoveFriendOption from "../../components/friendsComponents/RemoveFriendOption";
-import LoadingState from "../../assets/images/ui/Loading";
-import { colors } from "../../constants/colors";
-import EmptyState from "../../assets/images/ui/Empty";
 
-const LoadingFriends = () => (
-  <View>
-    <View style={styles.imgHolder}>
-      <LoadingState />
-    </View>
-    <Text style={styles.feedbackText}>
-      Sit tight, We are loading your Friends
-    </Text>
-  </View>
-);
-
-const EmptyFriends = () => (
-  <View>
-    <View style={styles.imgHolder}>
-      <EmptyState />
-    </View>
-    <Text style={styles.feedbackText}>No friends found</Text>
-  </View>
-);
 const AllFriendsScreen = () => {
   const [availableFriends, setAvailableFriends] = useState<FriendsResponse[]>(
     []
   );
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [showOptions, setShowOptions] = useState<Record<string, boolean>>({});
-  const [prevSelectedId, setPrevSelectedId] = useState<string | null>(null);
-
-  const optionClickHandler = (id: string) => {
-    setShowOptions((prevState) => {
-      if (prevSelectedId && prevSelectedId !== id) {
-        prevState[prevSelectedId] = false;
-      }
-      prevState[id] = !prevState[id];
-      return { ...prevState };
-    });
-    setPrevSelectedId((prevId) => (prevId === id ? null : id));
-  };
 
   useEffect(() => {
     const setFriendsData = async () => {
@@ -56,57 +19,14 @@ const AllFriendsScreen = () => {
         searchText: "",
       });
       setAvailableFriends(friends);
-      setShowOptions(
-        friends.reduce(
-          (acc: Record<string, boolean>, friend: FriendsResponse) => {
-            acc[friend.id] = false;
-            return acc;
-          },
-          {}
-        )
-      );
       setIsLoading(false);
     };
     setFriendsData();
   }, []);
 
   return (
-    <View>
-      <FlatList
-        data={availableFriends}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <FriendListItem
-            friendData={item}
-            ctaComponent={
-              <RemoveFriendOption
-                id={item.id}
-                showOptions={showOptions[item.id]}
-                optionClickHandler={optionClickHandler}
-              />
-            }
-          />
-        )}
-        contentContainerStyle={styles.contentContainer}
-        ListEmptyComponent={isLoading ? <LoadingFriends /> : <EmptyFriends />}
-      />
-    </View>
+    <FriendListItem friendsData={availableFriends} isLoading={isLoading} />
   );
 };
 
 export default AllFriendsScreen;
-
-const styles = StyleSheet.create({
-  contentContainer: {
-    paddingVertical: 8,
-  },
-  imgHolder: {
-    maxHeight: 450,
-  },
-  feedbackText: {
-    textAlign: "center",
-    color: colors.textPrimary,
-    fontSize: 18,
-    fontWeight: "500",
-  },
-});
